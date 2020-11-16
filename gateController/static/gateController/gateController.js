9041b2777d3074b07ba7cab337b8c014261b1005
Bicserdi Balazs <Balazs.Bicserdi@continental-corporation.com>
2020-11-16 21:58:06 +0100

    gate triggering added

--- gateController/static/gateController/gateController.js
+++ gateController/static/gateController/gateController.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $(window).on("orientationchange",function(){
+    $(window).on("orientationchange", function () {
         if ($(window).innerHeight() > $(window).innerWidth()) {
             $(".cust_col").removeClass("col-md-12").addClass("col-md-4");
 
@@ -33,6 +33,7 @@ function vehAccClicked() {
     $("#ped_acc_btn").prop("disabled", true);
     $("#svg_locked").show();
     $("#svg_unlocked").hide();
+    triggerGate("vehicle_access");
 }
 
 function pedAccClicked() {
@@ -40,4 +41,33 @@ function pedAccClicked() {
     $("#ped_acc_btn").prop("disabled", true);
     $("#svg_locked").show();
     $("#svg_unlocked").hide();
+    triggerGate("pedestrian_access");
+}
+
+function triggerGate(command) {
+    let token = getCookie("csrftoken");
+    $.ajax({
+        url: "triggerGate",
+        type: "POST",
+        data: { csrfmiddlewaretoken: token, command: command },
+        success: function (data) {
+            ;
+        },
+        error: function (jXHR, textStatus, errorThrown) {
+            alert(`There was an error communicating with the server: ${errorThrown}.`);
+        }
+    });
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
 }
--- gateController/views.py
+++ gateController/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
+from django.views.decorators.csrf import ensure_csrf_cookie
+from gpiozero import LED
+from time import sleep
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'gateController/gateController.html')
+
+def triggerGate(request):
+    led = LED(5)
+    led2 = LED(27)
+    led.off()
+    led2.off()
+
+    if request.method =='POST':
+        command = request.POST.get('command', '')
+        if command == "pedestrian_access":
+            led.on()
+            sleep(0.5)
+            led.off()
+            le2.off()
+            return HttpResponse(status=200)
+        elif command == "vehicle_access":
+            led2.on()
+            sleep(0.5)
+            led.off()
+            le2.off()
+            return HttpResponse(status=200)
+
+    led.off()
+    led2.off()
+    return HttpResponseBadRequest(status=400)
+    
